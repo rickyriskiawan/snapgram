b2823ac0a8a791303deb3aa657b5cdc71efa0126
Ricky <rickyrizkiawan666@gmail.com>
2025-03-08 17:22:05 +0700

    finished feat:sign up

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,8 @@
+import { z } from 'zod';
+
+export const SignupValidation = z.object({
+  name: z.string().min(3),
+  username: z.string().min(2),
+  email: z.string().email({ message: 'Invalid email address' }),
+  password: z.string().min(8, { message: 'Password must be at least 8 characters.' }),
+});
